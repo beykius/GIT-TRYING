8e3bd4bcc489bcd7d3a6a0afe70979dd76fd58e0
beykius <karolinafurmanaviciene@gmail.com>
2024-11-29 10:29:52 +0200

    delete button created

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ const body = document.querySelector('body');
 fetch('https://api.escuelajs.co/api/v1/users')
     .then(response => response.json())
     .then(data => {
-for (i = 0; i < data.length; i++) {
-    body.innerHTML += `
-    <div class="profile"><img src="${data[i].avatar}" alt="${data[i].id}">
-    <h2>${data[i].name}</h2>
-    <p><span class="label">Email:</span> ${data[i].email}</p>
-    <p><span class="label">Role:</span> ${data[i].role}</p>
-    <p><span class="label">Account Created:</span> ${data[i].creationAt}</p>
-    <p><span class="label">Last Updated:</span> ${data[i].updatedAt}</p></div>`
-}})
+        data.forEach(user => {
+            body.innerHTML += `
+    <div class="profile"><img src="${user.avatar}" alt="${user.id}">
+    <h2>${user.name}</h2>
+    <p><span class="label">Email:</span> ${user.email}</p>
+    <p><span class="label">Role:</span> ${user.role}</p>
+    <p><span class="label">Account Created:</span> ${user.creationAt}</p>
+    <p><span class="label">Last Updated:</span> ${user.updatedAt}</p>
+    <button>Delete</button>
+    </div>`
+        })
+
+
+        const buttons = document.querySelectorAll('button');
+        buttons.forEach(button => {
+            button.addEventListener('click', (e) => {
+                const profile = e.target.closest('.profile');
+                profile.remove();
+            })
+    })
+
+
+
+})
